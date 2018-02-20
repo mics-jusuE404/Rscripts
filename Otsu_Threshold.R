d6c0388f294825264b1484c5928b3ea76b1c8407
Alexander Toenges <a.toenges@uni-muenster.de>
2018-02-20 12:48:55 +0100

    Update Otsu_Threshold.R

--- Otsu_Threshold.R
+++ Otsu_Threshold.R
@@ -1,7 +1,9 @@
 #### Take a list of integers and find a threshold that splits these integers into two groups,
 #### aiming to minimize intragroup variance but maximize intergroup variance.
 #### Based on Otsu's threshold and code pretty much copied from <https://en.wikipedia.org/wiki/Otsu%27s_method>
-#### Modifications are the log10 transformation to make it suitable for larger numbers as common in NGS.
+#### Modifications are the log10 transformation to make it suitable for larger integers as common in NGS
+#### (log10 because Otsu was developped for 256-grayscale picture manipulation, and original code does not perform
+####  well without log10)
 #### Might be suitable if NGS data show strong bimodal distribution and any thresholding is desired (beyond by-eye methods)
 
 Otsu_NGS <- function(COUNTS){
