13e16f8c1291bc72fe9c54ace2952f52f862f0bf
Alexander Toenges <a.toenges@uni-muenster.de>
2018-03-25 22:12:34 +0200

    Update Get_CpG_Coordinates.R

--- Get_CpG_Coordinates.R
+++ Get_CpG_Coordinates.R
@@ -2,11 +2,13 @@
 #### Inspired by this post: https://support.bioconductor.org/p/95239/
 #### Written (or rather modified from the above post) by Alexander Toenges, 12/17:
 
-library(BSgenome.Hsapiens.UCSC.hg38) 
+###################################################################################
 require(Biostrings)
 require(parallel)
+###################################################################################
 
-Find_CpG <- function(Genome, Cores){
+Find_CpG <- function(Genome, Cores = 1){
+  
   if (class(Genome) != "BSgenome") stop("Genome must be a BSgenome!")
   
   CpG <- mclapply(seqlevels(Genome), function(x) start(matchPattern("CG", Genome[[x]])), mc.cores = Cores)
@@ -14,10 +16,11 @@ Find_CpG <- function(Genome, Cores){
     suppressWarnings(
       do.call(c, mclapply(1:length(seqlevels(Genome)), function(x) GRanges(names(Genome)[x], 
                                                                            IRanges(CpG[[x]], width = 2)
-                                                                           ), mc.cores=Cores))
+      ), mc.cores=Cores))
     )
   )
 }
 
 ## Example:
-hg38.CpG <- Find_CpG(Genome = BSgenome.Hsapiens.UCSC.hg38, Cores = 8)
+hg38.CpG  <- Find_CpG(Genome = BSgenome.Hsapiens.UCSC.hg38, Cores = 8)
+ecoli.CpG <- Find_CpG(Genome = BSgenome.Ecoli.NCBI.20080805, Cores = 2)
